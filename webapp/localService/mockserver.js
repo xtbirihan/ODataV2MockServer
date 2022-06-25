3c30576a6589bc93b534bf0e2fdee8d290073863
Hasan Tugay Birihan <hasantugay.birihan@nttdata.com>
2022-06-25 11:20:25 +0000

    Sample: OData V2 Mock Server - Step 2 - Creating a Mock Server to Simulate Data

---
+++ webapp/localService/mockserver.js
@@ -0,0 +1,23 @@
+sap.ui.define([
+    "sap/ui/core/util/MockServer",
+    "sap/base/Log"
+], function (MockServer, Log) {
+    "use strict";
+
+    return {
+        init: function () {
+            var oMockServer = new MockServer({
+                rootUri : "/"
+            });
+
+            oMockServer.simulate("../localService/metadata.xml", {
+                sMockdataBaseUrl:  "../localService/mockdata",
+                bGenerateMissingMockData: true
+            });
+
+            oMockServer.start();
+            Log.info("Running the app with mock data");            
+        }
+
+    };
+});
