dce79d214cb8bf66eec8f039162f60ee50540c95
Hasan Tugay Birihan <hasantugay.birihan@nttdata.com>
2022-06-25 11:40:57 +0000

    Sample: OData V2 Mock Server - Step 4 - Calling a Function Import

--- webapp/localService/mockserver.js
+++ webapp/localService/mockserver.js
@@ -1,7 +1,8 @@
 sap.ui.define([
+    "sap/ui/thirdparty/jquery",,
     "sap/ui/core/util/MockServer",
     "sap/base/Log"
-], function (MockServer, Log) {
+], function (jQuery, MockServer, Log) {
     "use strict";
 
     return {
@@ -14,6 +15,29 @@ sap.ui.define([
                 sMockdataBaseUrl:  "../localService/mockdata",
                 bGenerateMissingMockData: true
             });
+
+			var aRequests = oMockServer.getRequests();
+			aRequests.push({
+				method: "GET",
+				path: new RegExp("FindUpcomingMeetups(.*)"),
+				response: function(oXhr) {
+					Log.debug("Incoming request for FindUpcomingMeetups");
+					var today = new Date();
+					today.setHours(0); // or today.toUTCString(0) due to timezone differences
+					today.setMinutes(0);
+					today.setSeconds(0);
+					jQuery.ajax({
+						url: "/Meetups?$filter=EventDate ge " + "/Date(" + today.getTime() + ")/",
+						dataType : 'json',
+						async: false,
+						success : function(oData) {
+							oXhr.respondJSON(200, {}, JSON.stringify(oData));
+						}
+					});
+					return true;
+				}
+			});
+			oMockServer.setRequests(aRequests);
 
             var fnCustom = function(oEvent) {
 				var oXhr = oEvent.getParameter("oXhr");
